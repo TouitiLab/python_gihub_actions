a01ec2528e28478ff5d7239d5898bcc8b5b8896b
TouitiLab <touiti.abdelhak@gmail.com>
2024-10-05 19:39:51 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# example of python app
+
+print("Hello World!")
+
+from datetime import datetime
+
+# Get the current time
+current_time = datetime.now().time()
+
+# Print the time
+print("Current time:", current_time)
